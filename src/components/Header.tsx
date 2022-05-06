40520245d4245a064d68f2e0a1690d8b5c4d78ff
NtBat <na.than.fjv@gmail.com>
2022-05-06 17:09:05 -0300

    created back button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,24 @@
+import { ArrowLeft } from "phosphor-react";
 import { CloseButton } from "./CloseButton";
 
 interface HeaderProps {
   title: string;
   image?: string;
   alt?: string;
+  backButton?: () => void;
 }
 
-export function Header({ title, image, alt }: HeaderProps) {
+export function Header({ title, image, alt, backButton }: HeaderProps) {
   return (
     <header>
+      <button
+        type="button"
+        className="absolute top-5 left-5 text-zinc-400 hover:text-zinc-100"
+        onClick={backButton}
+      >
+        <ArrowLeft weight="bold" className="w-4 h-4" />
+      </button>
+
       <span className="text-xl leading-6 flex items-center gap-2">
         {image && <img src={image} alt={alt} className="w-6 h-6" />}
         {title}
--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -5,10 +5,12 @@ import FeedbackTypes from "../types";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackTypes;
+  onFeedbackRestartRequested: () => void;
 }
 
 export function FeedbackContentStep({
   feedbackType,
+  onFeedbackRestartRequested,
 }: FeedbackContentStepProps) {
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
@@ -18,6 +20,7 @@ export function FeedbackContentStep({
         title={feedbackTypeInfo.title}
         image={feedbackTypeInfo.image.source}
         alt={feedbackTypeInfo.image.alt}
+        backButton={onFeedbackRestartRequested}
       />
       <div className="flex py-8 gap-2 w-full"></div>
     </>
