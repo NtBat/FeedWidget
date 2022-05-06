3d2b3efb9a06c5a6d83c4fffecfc0770fa21ce89
NtBat <na.than.fjv@gmail.com>
2022-05-06 16:52:07 -0300

    move footer file

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export function Footer() {
+  return (
+    <footer className="text-xs text-neutral-400">
+      Feito com ♥ por{" "}
+      <a
+        className="underline underline-offset-2"
+        href="https://www.ntbatista.dev"
+        target="_blank"
+        rel="noopener"
+      >
+        NtBatista
+      </a>
+    </footer>
+  );
+}
